b2fb5c2c33ca5c370ac2ddb237b99303f78b95cd
C <earthhominid@gmail.com>
2022-10-21 12:58:41 -0300

    layout added

---
+++ components/Layout.tsx
@@ -0,0 +1,50 @@
+import Head from 'next/head';
+import Header from './header/Header';
+import Footer from './footer/Footer';
+
+interface Props {
+  title: string;
+  keywords: string;
+  description: string;
+  background: string;
+  children?: React.ReactNode;
+  mainPage: string;
+  currentPage: string;
+  mainPageTitle: string;
+  currentPageTitle: string;
+  styles: string;
+  width: string;
+}
+
+const Layout: React.FC<Props> = () => {
+  return (
+    <>
+      <Head>
+        <title></title>
+        <meta
+          name="description"
+          content="Barking by day, howling by night. Barker is a social platform to share howls, woofs, and barks."
+        />
+        <meta name="keywords" content="" />
+      </Head>
+
+      <header id="header">
+        <Header />
+      </header>
+
+      <footer id="footer">
+        <Footer />
+      </footer>
+    </>
+  );
+};
+
+export default Layout;
+
+Layout.defaultProps = {
+  title: 'Barker | Bark at the world',
+  description:
+    'Barking by day, howling by night. Barker is a social platform to share howls, woofs, and barks.',
+  keywords: `Chris Doll, barker, full stack developer
+  `,
+};
